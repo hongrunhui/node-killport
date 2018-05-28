69c4126ad46d2240725a5934f5d89d87d97b88af
hongrunhui <hongrunhui@baidu.com>
2018-05-28 20:34:22 +0800

    是用node

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const exec = require('child_process').exec;
+let port = process.argv[2] || 0;
+if (port === 0) {
+    console.log('输入参数错误，正确使用方法：kill 8888');
+    return;
+}
+const command = `sh ./killport.sh ${port}`;
+exec(command, (err, std, stderr) => {
+    if (!err) {
+        console.log(std);
+    }
+    else {
+        console.log('kill 端口失败，请重试');
+    }
+});
